63cbd9adb722ac7c57bc108ce7a5855f55482538
Samuele <s.fusaro@geniodiligence.it>
2021-03-08 20:05:12 +0100

    cloud backend address

--- app/src/main/java/com/example/industrial/Retrofit.java
+++ app/src/main/java/com/example/industrial/Retrofit.java
@@ -18,7 +18,7 @@ import retrofit2.http.Path;
 class APIClient {
 
 //    private static String BASE_URL = "http://10.0.2.2:5000";
-    private static String BASE_URL = "http://207.154.240.44:5000/";
+    private static String BASE_URL = "http://207.154.240.44:5000";
 
 
     private static Retrofit retrofitInstance = null;
