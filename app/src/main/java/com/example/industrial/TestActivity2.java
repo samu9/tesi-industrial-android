48ac5aaedc7c2923e93c18cd8cfdc5caf8bbd829
Samuele <s.fusaro@geniodiligence.it>
2021-03-14 19:28:11 +0100

    test activity

---
+++ app/src/main/java/com/example/industrial/TestActivity2.java
@@ -0,0 +1,14 @@
+package com.example.industrial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestActivity2 extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test2);
+    }
+}
