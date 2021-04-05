9edc5ccf0ec7070ea4f501a8a804606863f0ef37
Samuele <s.fusaro@geniodiligence.it>
2021-04-05 12:14:17 +0200

    added secrets external file management

--- app/src/main/java/com/example/industrial/API/APIClient.java
+++ app/src/main/java/com/example/industrial/API/APIClient.java
@@ -1,14 +1,22 @@
 package com.example.industrial.API;
 
+import com.example.industrial.BuildConfig;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static com.example.industrial.BuildConfig.API_KEY;
+import static com.example.industrial.BuildConfig.BASE_URL;
+
 public class APIClient {
-
-//    private static String BASE_URL = "http://10.0.2.2:5000";
-    private static String BASE_URL = "http://207.154.240.44:5000";
-
+    private final static String AUTHORIZATION_HEADER = "Authorization";
     private static Retrofit retrofitInstance = null;
 
     public static String getBaseUrl(){
@@ -17,10 +25,22 @@ public class APIClient {
 
     public static Retrofit getInstance() {
         if (retrofitInstance == null) {
+
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+            httpClient.addInterceptor(new Interceptor() {
+                @Override
+                public Response intercept(Chain chain) throws IOException {
+                    Request request = chain.request().newBuilder()
+                            .addHeader(AUTHORIZATION_HEADER, "Basic " + API_KEY).build();
+                    return chain.proceed(request);
+                }
+            });
             retrofitInstance = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addCallAdapterFactory(RxJava3CallAdapterFactory.create()) // serve a gestire la risposta di retrofit
                     .addConverterFactory(GsonConverterFactory.create()) // serializza e deserializza l'oggetto response
+                    .client(httpClient.build())
                     .build();
         }
         return retrofitInstance;
